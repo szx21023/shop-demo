8277ac78c72ad948845e9ad917d4dd418eb176ec
herry <herry@program.com.tw>
2022-02-22 18:08:02 +0800

    feat: login, logout

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
-from app.route import hello_world, index
+from app.route import hello_world, index, login, logout, home_page
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'secret'
     app.add_url_rule('/', '/', hello_world)
     app.add_url_rule('/index', 'index', index)
+    app.add_url_rule('/login', 'login', login)
+    app.add_url_rule('/logout', 'logout', logout)
+    app.add_url_rule('/home_page', 'home_page', home_page)
     return app
--- flask/app/route.py
+++ flask/app/route.py
@@ -1,7 +1,24 @@
-from flask import render_template
+from flask import render_template, request, session, redirect, url_for
 
 def hello_world():
     return "hello, mvc"
 
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+def login():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    session['username'] = username
+    return f"successful login!"
+
+def logout():
+    session.pop('username')
+    return redirect(url_for("index"))
+
+def home_page():
+    if 'username' in session:
+        return f"this is {session['username']} page!"
+
+    else:
+        return "none one page!"
