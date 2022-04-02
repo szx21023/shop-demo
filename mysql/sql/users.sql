27dc1887e421cfcfe363bf126d5a4cbac4474d68
herry <herry@program.com.tw>
2022-04-02 22:36:02 +0800

    mysql: create table user

--- mysql/sql/users.sql
+++ mysql/sql/users.sql
@@ -8,5 +8,9 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
-INSERT INTO `test`.`users` (`id`, `username`, `password`, `phone`, `email`, `create_time`) VALUES ('1', 'xiao', '123', '12345678910', '123@qq.com', '2020-04-10 01:22:07');
-INSERT INTO `test`.`users` (`id`, `username`, `password`, `phone`, `email`, `create_time`) VALUES ('2', 'fnong', '543', '87654321', '123@qq.com', '2020-04-11 02:24:25');
+INSERT INTO `test`.`users` (`id`, `username`, `password`, `phone`, `email`, `create_time`) VALUES ('1', 'xiao', '123', '0912345678', 'apple@qq.com', '2020-04-10 01:22:07');
+INSERT INTO `test`.`users` (`id`, `username`, `password`, `phone`, `email`, `create_time`) VALUES ('2', 'fnong', '543', '0923456781', 'banana@qq.com', '2020-04-11 02:24:25');
+INSERT INTO `test`.`users` (`id`, `username`, `password`, `phone`, `email`, `create_time`) VALUES ('3', 'chone', '981', '0934567812', 'orange@qq.com', '2020-04-12 13:07:49');
+INSERT INTO `test`.`users` (`id`, `username`, `password`, `phone`, `email`, `create_time`) VALUES ('4', 'bob', '746', '0945678123', 'lemon@qq.com', '2020-04-12 18:31:16');
+INSERT INTO `test`.`users` (`id`, `username`, `password`, `phone`, `email`, `create_time`) VALUES ('5', 'sana', '342', '0956781234', 'tomato@qq.com', '2020-04-13 07:44:23');
+INSERT INTO `test`.`users` (`id`, `username`, `password`, `phone`, `email`, `create_time`) VALUES ('6', 'elegan', '582', '0967812345', 'pieapple@qq.com', '2020-04-14 21:32:24');
